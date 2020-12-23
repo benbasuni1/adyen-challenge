cead0b1c32f3b8d6e911941b5936d7a500717271
Ben Basuni <benbasuni1@gmail.com>
2020-12-23 13:24:13 -0500

    added origin keys

--- origin_key_curl.sh
+++ origin_key_curl.sh
@@ -1,3 +1,9 @@
 curl https://checkout-test.adyen.com/v1/originKeys \
 -H "X-API-key: " \
--H "Content-Type: application/json"
+-H "Content-Type: application/json" \
+-d '{
+   "originDomains":[
+      "http://localhost:8080",
+      "https://adyen-challenge.herokuapp.com"
+   ]
+}'
